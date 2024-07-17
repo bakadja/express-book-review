63dcdd62b244064d0d86a491679f6cb48deb922a
ngongang <k.ngongang@addon-solution.de>
2024-07-17 15:25:12 +0200

    chore: Update user authentication mechanism and add validation to login route

--- router/auth_users.js
+++ router/auth_users.js
@@ -7,7 +7,6 @@ const { body,matchedData ,validationResult } = require("express-validator");
 
 let users = [];
 
-
 const authenticatedUser = (username,password)=>{ 
     return users.find(user => user.username === username && user.password === password) ;
 }
@@ -50,9 +49,48 @@ regd_users.post("/register",
 });
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login",
+  [
+    body("username")
+    .isString()
+    .withMessage("Username must be a string")
+    .isLength({min: 3})
+    .withMessage("Username must be at least 3 characters long")
+    .trim() 
+    .notEmpty()
+    .escape(),
+    body("password")
+    .isStrongPassword()
+    .withMessage("Password must be at least 8 characters long, contain at least 1 lowercase, 1 uppercase, 1 number and 1 special character")
+    .trim()
+    .notEmpty()
+    .escape()
+  ], 
+  (req,res) => {
+    
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      
+      const data = matchedData(req);
+      const {username, password} = data;
+
+      if(authenticatedUser(username, password)){
+        try {
+          // create a token
+          const token = jwt.sign({username: username}, process.env.JWT_SECRET, {expiresIn: "1h"});
+          req.session.authorization = {accessToken: token};
+
+          return res.status(200).json({message: "User logged in successfully"});
+          
+        } catch (error) {
+          return res.status(500).json({message: "Internal server error"});
+        }
+      } else {
+        return res.status(400).json({message: "Invalid username or password"});
+      }
+    }
+    return res.status(422).json({message: errors.array()});
 });
 
 // Add a book review
