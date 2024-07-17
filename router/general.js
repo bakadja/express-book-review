b0901acc9ae4d5f63d8cb718f074ba1dda34f729
ngongang <k.ngongang@addon-solution.de>
2024-07-17 15:25:01 +0200

    feat: Add function to get book list by value

--- router/general.js
+++ router/general.js
@@ -6,6 +6,25 @@ const public_users = express.Router();
 const { param, validationResult } = require('express-validator');
 
 
+
+
+const getBookByValue = (value) =>
+  new Promise((resolve, reject) => {
+    const bookList = {};
+    for (const [key, book] of Object.entries(books)) {
+      if (book[value] === value) {
+        bookList[key] = value;
+      }
+    }
+
+    if (Object.keys(bookList).length > 0) {
+      resolve(bookList);
+    } else {
+      reject({ message: "Author not found" });
+    }
+  });
+
+
 public_users.post("/register", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
@@ -41,54 +60,43 @@ public_users.get('/isbn/:isbn',
 // Get book details based on author
 public_users.get('/author/:author',
   param('author').notEmpty().escape() ,
-  (req, res) => {
+  async(req, res) => {
 
   const errors = validationResult(req);
 
-  if (errors.isEmpty()) {
-    
-    const author = req.params.author;
-    const bookList = {};
-    
-    for( const [key, value] of Object.entries(books)){
-      if (value.author === author){
-        bookList[key] = value;
-      }
-    }
-
-    if (Object.keys(bookList).length > 0) {
-      return res.status(200).json(bookList);
-    } else {
-      return res.status(404).json({message: "Author not found"});
-    }
+  if(!errors.isEmpty()) {
+    return res.status(422).json({message: errors.array()});
   }
-  return res.status(422).json({message: errors.array()});
+
+  const author = req.params.author;
+  try {
+    const bookList = await getBookByValue(author);
+    return res.status(200).json(bookList);
+  }
+  catch (error) {
+    return res.status(404).json({message: "Author not found"});
+  }
+  
 });
 
 // Get all books based on title
 public_users.get('/title/:title',
   param('title').notEmpty().escape(),
-  (req, res) => {
+  async(req, res) => {
   
   const errors = validationResult(req);
-
-  if (errors.isEmpty()) {
-    const title = req.params.title;
-    const bookList = {};
-    
-    for( const [key, value] of Object.entries(books)){
-      if (value.title === title){
-        bookList[key] = value;
-      }
-    }
-
-    if (Object.keys(bookList).length > 0) {
-      return res.status(200).json(bookList);
-    } else {
-      return res.status(404).json({message: "Title not found"});
-    }
+  if(!errors.isEmpty()) {
+    return res.status(422).json({message: errors.array()});
   }
-  return res.status(422).json({message: errors.array()});
+
+  const title = req.params.title;
+  try {
+    const bookList = await getBookByValue(title);
+    return res.status(200).json(bookList);
+  }
+  catch (error) {
+    return res.status(404).json({message: "Title not found"});
+  }
 });
 
 //  Get book review
@@ -98,19 +106,34 @@ public_users.get('/review/:isbn',
 
   const errors = validationResult(req);
 
-  if (errors.isEmpty()) {
-    const isbn = req.params.isbn;
+  if(!errors.isEmpty()) {
+
+    return res.status(422).json({message: errors.array()});
+  }
+
+  const isbn = req.params.isbn;
+
+  new Promise((resolve, reject) => {
     
     if (books[isbn]?.reviews) {
-      return res.status(200).json(books[isbn].reviews);
 
+      resolve(books[isbn].reviews);
     } else if (books[isbn]) {
-      return res.status(404).json({ message: "No reviews found" });
+
+      reject({ message: "No reviews found" });
     } else {
-      return res.status(404).json({ message: "Book not found" });
+
+      reject({ message: "Book not found" });
     }
-  }
-  return res.status(422).json({message: errors.array()});
+  })
+  .then((reviews) => {
+    return res.status(200).json(reviews);
+  })
+  .catch((error) => {
+    return res.status(404).json(error);
+  });
+    
+  
 });
 
 module.exports.general = public_users;
