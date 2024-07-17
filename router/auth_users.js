e60b7f94249314dfd7f0af24e7ea4791044e5e4d
ngongang <k.ngongang@addon-solution.de>
2024-07-17 15:38:01 +0200

    chore: Refactor user registration with validation and error handling

--- router/auth_users.js
+++ router/auth_users.js
@@ -11,42 +11,7 @@ const authenticatedUser = (username,password)=>{
     return users.find(user => user.username === username && user.password === password) ;
 }
 
-//register a user
-regd_users.post("/register",
-  [
-    body("username")
-    .isString()
-    .withMessage("Username must be a string")
-    .isLength({min: 3})
-    .withMessage("Username must be at least 3 characters long")
-    .trim() 
-    .notEmpty()
-    .escape(),
-    body("password")
-    .isStrongPassword()
-    .withMessage("Password must be at least 8 characters long, contain at least 1 lowercase, 1 uppercase, 1 number and 1 special character")
-    .trim()
-    .notEmpty()
-    .escape()
-  ],
-  (req,res) => {
-  
-  const errors = validationResult(req);
 
-  if (errors.isEmpty()) {
-    
-    const data = matchedData(req);
-    const {username, password} = data;
-
-    if(authenticatedUser(username, password)){
-      return res.status(400).json({message: "Username already exists"});
-    } else {
-      users.push(data);
-      return res.status(200).json({message: "User registered successfully"});
-    }
-  }
-  return res.status(422).json({message: errors.array()});
-});
 
 //only registered users can login
 regd_users.post("/login",
@@ -101,3 +66,4 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
 module.exports.authenticated = regd_users;
 module.exports.users = users;
+module.exports.authenticatedUser = authenticatedUser;
