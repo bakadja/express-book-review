e3f70d011a80985e293f80330bf4d8c79515684e
ngongang <k.ngongang@addon-solution.de>
2024-07-17 10:13:47 +0200

    chore: Update customer authentication mechanism

--- index.js
+++ index.js
@@ -3,15 +3,40 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",
+    session(
+        {
+            secret: process.env.SESSION_SECRET,
+            resave: true, 
+            saveUninitialized: true,
+            cookie: { 
+                secure: true,
+                httpOnly: true,
+                maxAge: 3600000
+             }
+        }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+        //authentification mechanism to check if user hat a valid token
+app.use("/customer/auth/*", (req,res,next) => {
+     const token = req.session.authorization?.accessToken;
+
+     if(token) {
+            jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+                if(err) {
+                    return res.status(403).json({message: "User not authenticated"});
+                }
+                req.user = user;
+                next();
+            });
+     } else {
+         return res.status(403).json({message: "User not logged in"});
+     }
 });
  
 const PORT =5500;
